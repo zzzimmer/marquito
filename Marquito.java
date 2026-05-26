e3876cf13197dd9341cc45ff39a59271e13d68c4
Julio <juliobrsouza@outlook.com>
2026-05-26 16:36:20 -0300

    priorização de inimigos que são considerados mais perigosos (usando a classe Enemy)

--- Marquito.java
+++ Marquito.java
@@ -4,10 +4,12 @@ import robocode.*;
 import robocode.util.Utils;
 import java.awt.Color;
 import java.awt.geom.Point2D;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Marquito extends AdvancedRobot {
-	String target;
-	double targetDistance = Double.POSITIVE_INFINITY;
+	Map<String, Enemy> enemies = new HashMap<>();
+	String targetName;
 	int moveDir = 1;
 
 	public void run() {
@@ -17,43 +19,77 @@ public class Marquito extends AdvancedRobot {
 		setAdjustRadarForRobotTurn(true);
 
 		while (true) {
-			target = null;
-			targetDistance = Double.POSITIVE_INFINITY;
-			setTurnRadarRightRadians(Double.POSITIVE_INFINITY);
+			chooseTarget();
 			if (nearWall()) {
 				moveDir = -moveDir;
 				setTurnRight(60);
 			}
 			setAhead(150 * moveDir);
+			doRadar();
+			doGun();
 			execute();
 		}
 	}
 
 	public void onScannedRobot(ScannedRobotEvent e) {
-		if (target == null || e.getDistance() < targetDistance || e.getName().equals(target)) {
-			target = e.getName();
-			targetDistance = e.getDistance();
+		Enemy en = enemies.get(e.getName());
+		if (en == null) en = new Enemy();
 
-			double absBearing = getHeadingRadians() + e.getBearingRadians();
-			double power = Math.clamp(400 / e.getDistance(), 1, 3);
-			double bulletSpeed = 20 - 3 * power;
+		double absBearing = getHeadingRadians() + e.getBearingRadians();
 
-			double enemyX = getX() + Math.sin(absBearing) * e.getDistance();
-			double enemyY = getY() + Math.cos(absBearing) * e.getDistance();
-			double time = Point2D.distance(getX(), getY(), enemyX, enemyY) / bulletSpeed;
-			double futureX = enemyX + Math.sin(e.getHeadingRadians()) * e.getVelocity() * time;
-			double futureY = enemyY + Math.cos(e.getHeadingRadians()) * e.getVelocity() * time;
-			double gunAngle = Math.atan2(futureX - getX(), futureY - getY());
+		en.name = e.getName();
+		en.energy = e.getEnergy();
+		en.distance = e.getDistance();
+		en.absBearing = absBearing;
+		en.heading = e.getHeadingRadians();
+		en.velocity = e.getVelocity();
+		en.x = getX() + Math.sin(absBearing) * e.getDistance();
+		en.y = getY() + Math.cos(absBearing) * e.getDistance();
+		en.lastSeen = getTime();
 
-			double radarTurn = Utils.normalRelativeAngle(absBearing - getRadarHeadingRadians());
+		enemies.put(en.name, en);
+	}
 
-			setTurnRadarRightRadians(radarTurn * 2);
-			setTurnGunRightRadians(Utils.normalRelativeAngle(gunAngle - getGunHeadingRadians()));
+	void chooseTarget() {
+		Enemy best = null;
+		double bestScore = Double.POSITIVE_INFINITY;
 
-			if (getGunHeat() == 0) {
-				setFire(power);
+		for (Enemy e : enemies.values()) {
+			if (getTime() - e.lastSeen > 20) continue;
+			double score = e.distance + e.energy * 10;
+			if (best == null || score < bestScore) {
+				best = e;
+				bestScore = score;
 			}
 		}
+
+		targetName = best == null ? null : best.name;
+	}
+
+	void doRadar() {
+		Enemy t = enemies.get(targetName);
+		if (t == null) {
+			setTurnRadarRightRadians(Double.POSITIVE_INFINITY);
+			return;
+		}
+		double turn = Utils.normalRelativeAngle(t.absBearing - getRadarHeadingRadians());
+		setTurnRadarRightRadians(turn * 2);
+	}
+
+	void doGun() {
+		Enemy t = enemies.get(targetName);
+		if (t == null) return;
+
+		double power = Math.clamp(400 / t.distance, 1, 3);
+		double bulletSpeed = 20 - 3 * power;
+		double time = Point2D.distance(getX(), getY(), t.x, t.y) / bulletSpeed;
+		double futureX = t.x + Math.sin(t.heading) * t.velocity * time;
+		double futureY = t.y + Math.cos(t.heading) * t.velocity * time;
+		double gunAngle = Math.atan2(futureX - getX(), futureY - getY());
+
+		setTurnGunRightRadians(Utils.normalRelativeAngle(gunAngle - getGunHeadingRadians()));
+
+		if (getGunHeat() == 0) setFire(power);
 	}
 
 	boolean nearWall() {
@@ -66,5 +102,22 @@ public class Marquito extends AdvancedRobot {
 
 	public void onHitByBullet(HitByBulletEvent e) {
 		moveDir = -moveDir;
+	}
+
+	public void onRobotDeath(RobotDeathEvent e) {
+		enemies.remove(e.getName());
+		if (e.getName().equals(targetName)) targetName = null;
+	}
+
+	static class Enemy {
+		String name;
+		double energy;
+		double distance;
+		double absBearing;
+		double heading;
+		double velocity;
+		double x;
+		double y;
+		long lastSeen;
 	}
 }
